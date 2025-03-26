7f68e4be0c2cb61813cac13c110387dfa047a5c6
davcika <daviskovals6@gmail.com>
2025-03-27 00:15:15 +0200

    kamer gruti

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # taskTracker
 This is my first project for roadmap.sh
+kamer ar json iet gruti
--- task_tracker_code.js
+++ task_tracker_code.js
@@ -1,22 +1,47 @@
+//apstrada json, vel japafixo!
+let fs = require("fs");
+var obj = {
+    table: []
+}
+const existingData = fs.readFileSync('./task_tracker_data.json')
+const jsonData = JSON.parse(existingData)
+
+//datums
+let now = new Date()
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let dateConf = [now.getDate(), months[now.getMonth()], now.getFullYear()]
+
+//nezinu
 const readline = require('node:readline');
+const { json } = require("node:stream/consumers");
+const { callbackify } = require("node:util");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
+//CLI komandu iegusana
 rl.question("Tasks_Tracker : ", command => {
-    console.log("command accepted")
+    let text = (isNaN(command.split(" ")[1])) ? command.split(" ")[1] : command.split(" ")[2];
+    let id  = (isNaN(command.split(" ")[1]) == false) ? command.split(" ")[1] : command.split(" ")[2];
+    command = command.split(" ")[0];
+
+    switch(command.toLowerCase()){
+        case "add":
+            jsonData.table.push({'desc': text, 'status': "todo", 'date': dateConf.toString().replace(/,/g, ' ')})
+            fs.writeFileSync('./task_tracker_data.json', JSON.stringify(jsonData))
+            break;
+
+        case "delete":
+            console.log("task deleted");
+            break;
+            
+        default:
+            console.log("wrong command");
+            break;
+    }
     rl.close()
 })
 
-switch(command.toLoverCase()){
-    case "add":
-        console.log("task added");
-        break;
-    case "delete":
-        console.log("task deleted")
-        break;
-    default:
-        console.log("wrong command")
-}
+
 
